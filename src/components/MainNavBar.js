5e9a45e26cc0a0982debc9787b9979403a0f41b6
Jeremy smith <jeremysmithseo@gmail.com>
2025-04-28 17:35:19 -0700

    added logo

--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -1,46 +1,59 @@
+// src/components/MainNavbar.jsx
 import React, { useState } from 'react';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
+  NavbarToggler,
+  Collapse,
   Nav,
   NavItem,
   NavLink,
   Container
 } from 'reactstrap';
-import "../styles/Home.module.css"
+import Image from 'next/image';
+import logo from '../../src/images/logo.png'; // adjust path as needed
 
-function MainNavbar() {
+const MainNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <Navbar color="dark" dark expand="md" className="py-3">
-      <Container>
-        <NavbarBrand href="/">
-          {/* Replace with your logo image or text */}
-          {/* <img src="/images/logo.png" alt="Logo" height="40" /> */}
-          
+    <Navbar color="light" light expand="md" className="py-3 shadow-sm">
+      <Container fluid className="px-4">
+        <NavbarBrand href="/" className="d-flex align-items-center">
+          <Image
+            src={logo}
+            alt="Logo"
+            width={140}
+            height={50}
+            style={{ height: 'auto', width: 'auto', maxHeight: '150px' }}
+            priority
+          />
         </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="ms-auto" navbar>
+          <Nav className="ms-auto d-flex align-items-center" navbar>
             <NavItem>
-              <NavLink href="/about">About Us</NavLink>
+              <NavLink href="/about" className="px-3">
+                About Us
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/programs">Programs</NavLink>
+              <NavLink href="/programs" className="px-3">
+                Programs
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/contact">Contact</NavLink>
+              <NavLink href="/contact" className="px-3">
+                Contact
+              </NavLink>
             </NavItem>
           </Nav>
         </Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default MainNavbar;
