e740e4763c43e81c9e02329f1be58f514bbb2822
Jeremy smith <jeremysmithseo@gmail.com>
2025-06-30 10:26:52 -0700

    wip

--- src/pages/blog/blog7.js
+++ src/pages/blog/blog7.js
@@ -15,42 +15,44 @@ export default function TryoutsPost() {
       <Container className="mt-5">
         <Row>
           <Col md={{ size: 10, offset: 1 }}>
-            <h1>Tips for Tryouts: How to Stand Out and Make the Team</h1>
-            <p>
+            <h1 className="mb-4" style={{ fontSize: '2.5rem' }}>
+              Tips for Tryouts: How to Stand Out and Make the Team
+            </h1>
+            <p className="fs-5">
               Have you ever been in a situation where you desperately wanted to make a team, but you
               had to go through a grueling tryout period? In the tryout, you are accompanied by 40
               or 50 players with the exact same goal.
             </p>
-            <p>
+            <p className="fs-5">
               Included in the group of hopefuls are players that fall into five categories:
             </p>
-            <ol>
+            <ol className="fs-5">
               <li>the ones who will surly make the team and know it:</li>
               <li>the ones who will surly make the team but don’t think they will:</li>
               <li>the ones who have a 50 – 50 chance of making the team:</li>
               <li>the ones with no chance but think they do:</li>
               <li>the ones with no chance and know it:</li>
             </ol>
-            <p>
+            <p className="fs-5">
               Let’s assume you fall into category No. 3. Your chances of making the team rest solely on
               your performance in the tryout – a real scary thought for some. Since you understand
               completely that it’s all up to you, being as prepared as you can, should be your number one
               focus. So, to ensure that you enter the tryout with every advantage you can, here are my tips
               for you.
             </p>
-            <h2>1 GET FIT</h2>
-            <p>
+            <h2 className="mt-4" style={{ fontSize: '1.75rem' }}>1 GET FIT</h2>
+            <p className="fs-5">
               Let’s face it; there is only one aspect of this situation that you can completely control.
               You can’t manipulate the coach. You can’t dictate how the other players will perform, and
               you can’t even depend on your own skills at any given time. All you can control is your fitness.
             </p>
-            <p>
+            <p className="fs-5">
               If you show up to the tryout and your not in top physical form, your chances have gone from
               50 – 50 to 30 – 70 before you’ve even touched a ball. The choice is up to you – you can work
               hard to get into the best form you can before tryout day(s), or can use the first session of the
               tryout to get yourself into some kind of playing shape. You alone can determine your fitness.
             </p>
-            <p>
+            <p className="fs-5">
               Showing up in top form gives you three immediate advantages. First, you have shown the coaches
               that you are taking it seriously and you have worked hard to make their team. Second, while
               other players are sending heavy – legged passes at your forehead or out of bounds, and are
@@ -59,82 +61,82 @@ export default function TryoutsPost() {
               Third, you have eliminated any confidence problems you might have had entering the tryout.
               Being in top form is a excellent confidence builder.
             </p>
-            <h2>2 SHOW YOUR STRENGTHS</h2>
-            <p>
+            <h2 className="mt-4" style={{ fontSize: '1.75rem' }}>2 SHOW YOUR STRENGTHS</h2>
+            <p className="fs-5">
               A common mistake players make in a tryout environment is they forget whythey were invited or at
               the tryout in the first place. There is something about every player that makes them special.
               Take the time, take a few minutes to figure out what makes you a special player. Make a list,
               and be sure to work on your strengths, everyday, before the tryouts. Tomorrow is not today.
               Your going to need them.
             </p>
-            <p>
+            <p className="fs-5">
               It might be dribbling that sets you apart. You may be an above average header of the ball. You
               might have a long throw – in. You might be a great organizer, a slick passer or maybe you have
               a powerful and accurate finishing shot. You might be a tenacious defender or hard tackeler.
               Perhaps you are able to read the game well, set the pace, or run all day. Whatever your strength
               is, make sure you show it everyday at the tryout.
             </p>
-            <p>
+            <p className="fs-5">
               A coach may be looking for a player with certain skills to fit into a complementary role for a
               player he or she has already selected. Or there may be a serious need to fill within the team,
               and the coach may be pre – occupied with finding someone to fill that role.
             </p>
-            <h2>3 REMAIN POSITIVE</h2>
-            <p>
+            <h2 className="mt-4" style={{ fontSize: '1.75rem' }}>3 REMAIN POSITIVE</h2>
+            <p className="fs-5">
               There is not a coach in the world who is looking for the player whose main strength is whining
               and complaining. How many times have you heard this: “She doesen’t have much skill, but boy can
               she complain…. there is certainly a place on this team for a player like that.”
             </p>
-            <p>
+            <p className="fs-5">
               So keep your snide comments to yourself, and be sure to encourage everyone, even those who
               are trying to beat you out. There is also nothing more annoying than the player who is late
               arriving for the training session or drill, holding everyone up while he or she saunters over at
               the speed of a glacier.
             </p>
-            <h2>4 BE CONSISTENT</h2>
-            <p>
+            <h2 className="mt-4" style={{ fontSize: '1.75rem' }}>4 BE CONSISTENT</h2>
+            <p className="fs-5">
               You never know when someone is watching. There is no drill or water break so unimportant that
               it doesn’t deserve your full effort and attention. If you get bored with a drill, then find a way
               to get un – bored fast. Coaches are not looking for players, they have to constantly motivate.
               That energy is saved for dealing with the players in category No. 1 above.
             </p>
-            <h2>5 DON'T SPECIALIZE</h2>
-            <p>
+            <h2 className="mt-4" style={{ fontSize: '1.75rem' }}>5 DON'T SPECIALIZE</h2>
+            <p className="fs-5">
               There will come a time when you are asked what position to play. While all your fellow players
               are telling the coach exactly where they should be placed on the field, you should let the coach
               know where you played in the past, but that you are willing to play anywhere ….or that you
               are willing to learn anything to make the team.
             </p>
-            <p>
+            <p className="fs-5">
               When you advance to the next level, chances are pretty good you will need to play a new position.
               It’s also very rare for two different coaches to look at the same player and see the exact same
               qualities. Your old coach may have preferred you to play outside mid, while your new coach may
               see some qualities of a central defender in you.
             </p>
-            <p>
+            <p className="fs-5">
               In today’s game, there are no real single – position players any longer. You need attacking skills
               to play in the back, you need defending skills to play up top, and you need everything and then
               some to compete in the mid - field. It’s almost certain that out of a 100 players, at least 75
               will say they are central midfielders. All but two will have to play a different position if they
               are to make the team. Knowing this up front gives you an advantage.
             </p>
-            <p>
+            <p className="fs-5">
               Be flexable. Remember, if you can only play one position, you better be the absolute best at that
               position.
             </p>
-            <h2>6 PLAY SMART</h2>
-            <p>
+            <h2 className="mt-4" style={{ fontSize: '1.75rem' }}>6 PLAY SMART</h2>
+            <p className="fs-5">
               No one is expecting you to beat every player on then field every time you touch the ball. There
               are times when you should and times where you shouldn’t. And you already know those moments.
               Make sure the coach is duly impressed, but don’t overdo it.
             </p>
-            <h2>7 PAY ATTENTION</h2>
-            <p>
+            <h2 className="mt-4" style={{ fontSize: '1.75rem' }}>7 PAY ATTENTION</h2>
+            <p className="fs-5">
               Don’t be high maintenance. If a coach is explaining a drill or activity, watch and listen so you
               are able to understand it without asking a bunch of questions.
             </p>
-            <h2>8 RELAX</h2>
-            <p>
+            <h2 className="mt-4" style={{ fontSize: '1.75rem' }}>8 RELAX</h2>
+            <p className="fs-5">
               If you are tense and nervous, your performance will show it. You can’t concentrate on preparing
               the ball if you are concerned with outside influences.
             </p>
